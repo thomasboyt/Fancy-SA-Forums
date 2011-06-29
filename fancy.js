74be411e4973d914ff3e0efd7d4c4b07ae1ae77f
Thomas Boyt <me@thomasboyt.com>
2011-06-29 01:09:13 -0400

    more stuff, added readme

--- fancy.js
+++ fancy.js
@@ -1,8 +1,8 @@
-// Removes the log out link from above the forums index
-if (window.location.href == "http://forums.somethingawful.com/" || window.location.href == "http://forums.somethingawful.com/index.php") {
-	$(".mainbodytextsmall").hide();
-}
-
 $(".forumbar").append($("#ac_timemachine"));
 
-$("#nav_purchase").before("<div id='header'><img src='http://i.somethingawful.com/core/head-logo-bluegren.png' /></div>")
+$("#container").prepend("<div id='header'><img src='http://i.somethingawful.com/core/head-logo-bluegren.png' /></div>")
+
+if (window.location.href.indexOf("search") != -1) {
+	$("#globalmenu").after("<div id='container'>dd");
+	$("#copyright").after("</div>");
+}
---
+++ readme.md
@@ -0,0 +1,18 @@
+Fancy SA Forums is a Chrome extension that makes the Something Awful Forums look prettier.
+
+### To-do
+
+#### Pages to redesign
+* Search page
+* User CP
+* Post/reply page
+	* Large warning if no post icon is chosen
+	* Better font in reply box
+
+##### Small cosmetic issues
+* Header banner in YOSPOS
+* Breadcrumb trail hover is inconsistent
+
+#### Other
+* Fix Chrome alignment issues
+* Include Chemo Clinic & Gentle Autist's TIMG fix
