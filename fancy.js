84e6db47fda8b54c2a2c5374d05cd73c7e847bcf
Thomas Boyt <me@thomasboyt.com>
2011-06-30 06:03:37 -0400

    removed banner in yospos, new page nav position, misc.

--- fancy.js
+++ fancy.js
@@ -1,8 +1,30 @@
 $(".forumbar").append($("#ac_timemachine"));
 
-$("#container").prepend("<div id='header'><img src='http://i.somethingawful.com/core/head-logo-bluegren.png' /></div>")
+/* No banner in YOSPOS */
+try {
+	if ($(".breadcrumbs").html().indexOf("YOSPOS") == -1) {
+		$("#container").prepend("<div id='header'><img src='http://i.somethingawful.com/core/head-logo-bluegren.png' /></div>")
+	}
+}
+catch(e) {
+	//no breadcrumbs
+	$("#container").prepend("<div id='header'><img src='http://i.somethingawful.com/core/head-logo-bluegren.png' /></div>")
+}
 
 if (window.location.href.indexOf("search") != -1) {
 	$("#globalmenu").after("<div id='container'>dd");
 	$("#copyright").after("</div>");
+}
+
+/* Experimental: new page nav positioning */
+
+if (window.location.href.indexOf("forumdisplay") != -1)  {
+	//move top page nav down to above blue bar
+	$(".pages:first").insertBefore($("#forum"));
+	$(".pages:first").attr("class", "pages top");
+}
+
+if (window.location.href.indexOf("showthread.php") != -1 ) {
+	//move bottom page nav up to under the post/reply buttons
+	$(".pages:last").insertAfter($(".threadbar.bottom"))
 }
