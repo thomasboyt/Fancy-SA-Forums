f5a1e7a826153750895cb419740e090252ecc25d
Thomas Boyt <me@thomasboyt.com>
2011-07-04 19:44:36 -0400

    no really i fixed it this time i think!!

--- tablebreak.js
+++ tablebreak.js
@@ -7,20 +7,21 @@ jQuery.expr[':'].parents = function(a,i,m){
     return jQuery(a).parents(m[3]).length < 1;
 };
 
-$(".postbody img.img").bind("load", function(e) {
-	
+$(".postbody img.img").one("load", function() {
 	console.log("image loaded")
 	console.log("image: " + $(this).attr("src") + " has width " + $(this).width());
-	
+
 	// todo: replace 700 pixels with the content div's width (so it is the max possible size)
-	
+
 	if ($(this).width() > 700) {
 		$(this).maxSize({
 			width: 700
 		});
-	
+
 		$(this).filter(":parents(a)").wrap("<a href='" + $(this).attr("src") + "' />");
-		
+	
 		console.log("resized " + $(this).attr("src"));
 	}
+}).each(function() {
+	if(this.complete) $(this).load();
 });
