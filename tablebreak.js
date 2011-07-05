b1cad45911d57ac85487ba36245a2c21db769524
Thomas Boyt <me@thomasboyt.com>
2011-07-05 12:23:27 -0400

    fix for cached images, hopefully

--- tablebreak.js
+++ tablebreak.js
@@ -1,28 +1,32 @@
 /* stop breaking the fucking tables yall */
 
-// thanks to Paolo Bergantino & Nick Craver
+// thanks to Paolo Bergantino & Andrew Ramdsen
 // http://stackoverflow.com/questions/965816/what-jquery-selector-excludes-items-with-a-parent-that-matches-a-given-selector/965962#965962
-// http://stackoverflow.com/questions/3877027/jquery-callback-on-image-load-even-when-the-image-is-cached
+// http://irama.org/news/2011/06/05/cached-images-have-no-width-or-height-in-webkit-e-g-chrome-or-safari/
 
 jQuery.expr[':'].parents = function(a,i,m){
     return jQuery(a).parents(m[3]).length < 1;
 };
 
-$(".postbody img.img").one("load", function() {
-	console.log("image loaded")
-	console.log("image: " + $(this).attr("src") + " has width " + $(this).width());
-
-	// todo: replace 700 pixels with the content div's width (so it is the max possible size)
-
-	if ($(this).width() > 700) {
-		$(this).maxSize({
-			width: 700
-		});
-
-		$(this).filter(":parents(a)").wrap("<a href='" + $(this).attr("src") + "' />");
+$(".postbody img.img").load(function() {
+	var img = this;
 	
-		console.log("resized " + $(this).attr("src"));
-	}
-}).each(function() {
-	if(this.complete) $(this).load();
+	//webkit cache'd image width hack
+	setTimeout(function() {
+		var src = $(this).attr('src');
+	
+		console.log("image: " + $(img).attr("src") + " has width " + $(img).width());
+
+		// todo: replace 700 pixels with the content div's width (so it is the max possible size)
+
+		if ($(img).width() > 700) {
+			$(img).maxSize({
+				width: 700
+			});
+
+			$(img).filter(":parents(a)").wrap("<a href='" + $(img).attr("src") + "' />");
+	
+			console.log("resized " + $(img).attr("src"));
+		}
+	}, 0);
 });
