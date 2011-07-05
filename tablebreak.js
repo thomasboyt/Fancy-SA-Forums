a502f9a8ebc7ebe2586d1cd81bb28332d312b8b2
Thomas Boyt <me@thomasboyt.com>
2011-07-05 12:26:27 -0400

    whoops, turned out i needed nick craver's bit after all

--- tablebreak.js
+++ tablebreak.js
@@ -1,14 +1,15 @@
 /* stop breaking the fucking tables yall */
 
-// thanks to Paolo Bergantino & Andrew Ramdsen
+// thanks to Paolo Bergantino, Nick Craver & Andrew Ramdsen
 // http://stackoverflow.com/questions/965816/what-jquery-selector-excludes-items-with-a-parent-that-matches-a-given-selector/965962#965962
+// http://stackoverflow.com/questions/3877027/jquery-callback-on-image-load-even-when-the-image-is-cached
 // http://irama.org/news/2011/06/05/cached-images-have-no-width-or-height-in-webkit-e-g-chrome-or-safari/
 
 jQuery.expr[':'].parents = function(a,i,m){
     return jQuery(a).parents(m[3]).length < 1;
 };
 
-$(".postbody img.img").load(function() {
+$(".postbody img.img").one('load', function() {
 	var img = this;
 	
 	//webkit cache'd image width hack
@@ -29,4 +30,6 @@ $(".postbody img.img").load(function() {
 			console.log("resized " + $(img).attr("src"));
 		}
 	}, 0);
+}).each(function() {
+		if(this.complete) $(this).load();
 });
